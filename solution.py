28626a41fb33a23bd45bc19e220619c9d32821bd
Viktoria Marchenko <92745008+vsmarrch@users.noreply.github.com>
2023-04-02 14:15:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 260376781 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    y = np.log(x - 199)
+    return y.mean() # Ваш ответ
